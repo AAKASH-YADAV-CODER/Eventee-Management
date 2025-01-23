13940a669558e20c925c7978e437524c8e65a8e5
Aakash Yadav <ay614838@gmail.com>
2025-01-23 12:03:22 +0530

    update codebase

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,15 +8,14 @@ app.use(
     origin: process.env.ORIGIN_URL_CORS,
   })
 );
-app.use(express.json({ limit: "16kbs" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kbs" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 //import Routes
 import userRouter from "./routes/user.route.js";
-
 //Route Declaration
-app.use("/api/v1/users", userRouter);
+app.use("/user", userRouter);
 
 export { app };
